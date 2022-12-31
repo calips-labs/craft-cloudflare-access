03ba5c8e838b20f8d914cc6a761b80d28b710417
Geert Wirken <geert@calips.nl>
2022-12-31 16:25:01 +0100

    Add settings model and form

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -4,10 +4,56 @@ namespace calips\cfaccess\models;
 
 use Craft;
 use craft\base\Model;
+use craft\helpers\App;
 
 /**
  * Cloudflare Access settings
  */
 class Settings extends Model
 {
+    public bool $enable = false;
+    public bool $enforce = false;
+    public ?string $issuer = null;
+    public ?string $aud = null;
+
+    public function defineRules(): array
+    {
+        return [
+            [['enable', 'enforce'], 'boolean'],
+            [['issuer', 'aud'], 'required', 'when' => function(self $model) { return $model->enable; }],
+            [['issuer', 'aud'], 'string'],
+        ];
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIssuer(): ?string
+    {
+        return App::parseEnv($this->issuer);
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAud(): ?string
+    {
+        return App::parseEnv($this->aud);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnable(): bool
+    {
+        return App::parseBooleanEnv($this->enable);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnforce(): bool
+    {
+        return App::parseBooleanEnv($this->enforce);
+    }
 }
