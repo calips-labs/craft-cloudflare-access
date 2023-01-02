62413552f27c601b881803c3f4541b50984fab90
Geert Wirken <geert@calips.nl>
2023-01-02 22:04:42 +0100

    Add config file

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Cloudflare Access config.php
+ *
+ * This file exists only as a template for the Cloudflare Access settings.
+ * It does nothing on its own.
+ *
+ * Don't edit this file, instead copy it to 'craft/config' as 'cloudflare-access.php'
+ * and make your changes there to override default settings.
+ *
+ * Once copied to 'craft/config', this file will be multi-environment aware as
+ * well, so you can have different settings groups for each environment, just as
+ * you do for 'general.php'
+ */
+
+return [
+    '*' => [
+        // Issuer URL (Cloudflare team domain)
+        // 'issuer' => '<team>.cloudflareaccess.com',
+        // Audience tag
+        // 'aud' => 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        // Automatically login to CP
+        // 'autologin_cp' => true,
+        // Automatically login to frontend
+        // 'autologin_frontend' => true,
+    ],
+];
